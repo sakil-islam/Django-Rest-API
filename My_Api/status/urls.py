829f8d6f7ac07e1df75b8c7d48d34d5f3a3d7419
Sakil Islam <sakilislam1080@gmail.com>
2021-05-28 22:12:23 +0600

    List Create APIView

--- My_Api/status/urls.py
+++ My_Api/status/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import StatusAPIView
+from .views import StatusAPIView, StatusListAPIView, StatusCreateAPIView
 
 urlpatterns = [
     path("status/", StatusAPIView.as_view()),
+    path("status/list/", StatusListAPIView.as_view()),
+    path("status/create/", StatusCreateAPIView.as_view()),
+
 ]
