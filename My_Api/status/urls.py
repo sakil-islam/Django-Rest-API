a5fff1988941ff56fa2f21f6f0abb2a658a32e1a
Sakil Islam <sakilislam1080@gmail.com>
2021-05-29 10:01:56 +0600

    Detail, Delete, Update APIVIEW

--- My_Api/status/urls.py
+++ My_Api/status/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import StatusAPIView, StatusListAPIView, StatusCreateAPIView
+from .views import (
+    StatusAPIView,
+    StatusListAPIView,
+    StatusCreateAPIView,
+    StatusDetailAPIView,
+    StatusUpdateAPIView,
+    StatusDeleteAPIView,
+)
 
 urlpatterns = [
     path("status/", StatusAPIView.as_view()),
     path("status/list/", StatusListAPIView.as_view()),
     path("status/create/", StatusCreateAPIView.as_view()),
-
+    path("status/detail/<id>/", StatusDetailAPIView.as_view()),
+    path("status/update/<id>/", StatusUpdateAPIView.as_view()),
+    path("status/delete/<id>/", StatusDeleteAPIView.as_view()),
 ]
