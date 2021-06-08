3077f94e6a8f92d79d243ee846042894612c2f92
Sakil Islam <sakilislam1080@gmail.com>
2021-06-08 23:51:28 +0600

    Mixins

--- My_Api/status/urls.py
+++ My_Api/status/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path
 from .views import (
-    StatusAPIView,
-    StatusListAPIView,
-    StatusCreateAPIView,
     StatusDetailAPIView,
-    StatusUpdateAPIView,
-    StatusDeleteAPIView,
+    StatusListCreateView,
 )
 
+# status/ -> List, Create => GET, POST
+# status/<id>/ -> Details, Delete, Update => GET, DELETE, PUT/PATCH
+
 urlpatterns = [
-    path("status/", StatusAPIView.as_view()),
-    path("status/list/", StatusListAPIView.as_view()),
-    path("status/create/", StatusCreateAPIView.as_view()),
-    path("status/detail/<id>/", StatusDetailAPIView.as_view()),
-    path("status/update/<id>/", StatusUpdateAPIView.as_view()),
-    path("status/delete/<id>/", StatusDeleteAPIView.as_view()),
+    path("status/", StatusListCreateView.as_view()),
+    path("status/<id>/", StatusDetailAPIView.as_view()),
 ]
