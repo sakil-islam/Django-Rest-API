f07c8e499ec0497b851ab7fe7f008b48c6f03851
Sakil Islam <sakilislam1080@gmail.com>
2021-06-08 23:57:38 +0600

    CRUD with 2 End Points

--- My_Api/status/views.py
+++ My_Api/status/views.py
@@ -9,23 +9,12 @@ from rest_framework import generics, mixins
 # Create your views here.
 
 
-class StatusListCreateView(mixins.CreateModelMixin, generics.ListAPIView):
+class StatusListCreateView(generics.ListCreateAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
-
-class StatusDetailAPIView(
-    mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.RetrieveAPIView
-):
+class StatusDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
     lookup_field = "id"
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
